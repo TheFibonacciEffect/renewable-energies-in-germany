92d27b2050900b69273d1b09476f8c96a7de905b
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-12 15:11:36 +0100

    plot average percentage

--- vis.jl
+++ vis.jl
@@ -9,8 +9,9 @@ ws = [14,15,16]
 
 coaletc = data[5]["data"] .+ data[6]["data"] .+ data[8]["data"]
 renew = data[14]["data"] .+ data[15]["data"] .+ data[16]["data"]
-
-plot!(p, data[1]["xAxisValues"], coaletc, label="Coal etc")
-plot!(p, data[1]["xAxisValues"], renew, label="Renewables")
-plot!(p, data[1]["xAxisValues"], coaletc .+ renew , label="Total")
-p
+tot = coaletc .+ renew
+# plot!(p, data[1]["xAxisValues"], coaletc./tot, label="Coal etc")
+plot!(p, data[1]["xAxisValues"], renew./tot, label="Renewables/(Renewables+Coal+Gas)")
+avg = sum(renew)/sum(tot)
+plot!(p, data[1]["xAxisValues"], avg*ones(length(data[1]["xAxisValues"])), label="Average", linestyle=:dash)
+savefig(p, "renewables proportion.png")
