24d4ef704034151c9bdfd046e86421a1b4a4c72d
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-12 15:32:51 +0100

    use whole year

--- vis.jl
+++ vis.jl
@@ -1,7 +1,7 @@
 using JSON
 using Plots
 
-data = JSON.parsefile("./frauenhofer july 2023.json")
+data = JSON.parsefile("./year2023.json")
 
 p =  plot(x=data[1]["xAxisValues"],)
 bse = [5,6,8]
@@ -12,6 +12,7 @@ renew = data[14]["data"] .+ data[15]["data"] .+ data[16]["data"]
 tot = coaletc .+ renew
 # plot!(p, data[1]["xAxisValues"], coaletc./tot, label="Coal etc")
 plot!(p, data[1]["xAxisValues"], renew./tot, label="Renewables/(Renewables+Coal+Gas)")
-avg = sum(renew)/sum(tot)
+@show avg = sum(renew)/sum(tot)
 plot!(p, data[1]["xAxisValues"], avg*ones(length(data[1]["xAxisValues"])), label="Average", linestyle=:dash)
 savefig(p, "renewables proportion.png")
+p
