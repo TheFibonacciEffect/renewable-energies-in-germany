42a3b77505c8871030e8d9d2c8a14acbd5c89805
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-12 15:06:53 +0100

    plot coal + gas + renew

--- vis.jl
+++ vis.jl
@@ -6,11 +6,11 @@ data = JSON.parsefile("./frauenhofer july 2023.json")
 p =  plot(x=data[1]["xAxisValues"],)
 bse = [5,6,8]
 ws = [14,15,16]
-for key in [bse ws]
-    @show key
-    source = data[key]
-    @show name = source["name"]["de"]
-    plot!(p, source["data"], label=name, legend=:topleft)
-end
 
+coaletc = data[5]["data"] .+ data[6]["data"] .+ data[8]["data"]
+renew = data[14]["data"] .+ data[15]["data"] .+ data[16]["data"]
+
+plot!(p, data[1]["xAxisValues"], coaletc, label="Coal etc")
+plot!(p, data[1]["xAxisValues"], renew, label="Renewables")
+plot!(p, data[1]["xAxisValues"], coaletc .+ renew , label="Total")
 p
