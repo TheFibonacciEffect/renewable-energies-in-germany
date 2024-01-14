63925a5656bd950ab4f3add8d83a5d596e09e66f
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-14 15:04:06 +0100

    clean up a little

--- vis.jl
+++ vis.jl
@@ -3,9 +3,6 @@ using Plots
 
 data = JSON.parsefile("./year2023.json")
 
-p =  plot(x=data[1]["xAxisValues"],)
-bse = [5,6,8]
-ws = [14,15,1      ]
 
 # "Pumpspeicher Verbrauch"                  1
 # "Grenzüberschreitender Stromhandel"       2
@@ -31,20 +28,26 @@ ws = [14,15,1      ]
 
 for key in [15,16,17,5,4,10,11,12,13]
     source = data[key]
-    # @show name = source["name"]["de"] , key
     println(source["name"]["en"], " ", key)
 end
 
+# define the groups of the sources
 coaletc = data[6]["data"] .+ data[7]["data"] .+ data[8]["data"] + data[9]["data"]
 renew = data[15]["data"] .+ data[16]["data"] .+ data[17]["data"] + data[5]["data"] .+ data[4]["data"] .+ data[10]["data"] .+ data[11]["data"] .+ data[12]["data"]
 other = data[13]["data"]
 wind_solar = data[15]["data"] + data[16]["data"] + data[17]["data"] 
 wind_solar_storage = data[15]["data"] + data[16]["data"] + data[17]["data"]  + data[11]["data"] .+ data[12]["data"]+ data[1]["data"]
-tot = coaletc .+ renew .+ other
-# plot!(p, data[1]["xAxisValues"], coaletc./tot, label="Coal etc")
-plot!(p, data[1]["xAxisValues"], renew./tot, label="Renewables/(Renewables+Coal+Gas)")
-@show avg = sum(renew)/sum(tot)
-@show min = minimum(renew./tot)
+combined_renew_coal_other = coaletc .+ renew .+ other
+
+all_except_nuclear = float(zeros(length(data[1]["xAxisValues"])))
+for i in [1:2; 4:17]
+    all_except_nuclear .+= data[i]["data"]
+end
+
+p =  plot(x=data[1]["xAxisValues"],)
+plot!(p, data[1]["xAxisValues"], renew./combined_renew_coal_other, label="Renewables/(Renewables+Coal+Gas)")
+@show avg = sum(renew)/sum(combined_renew_coal_other)
+@show min = minimum(renew./combined_renew_coal_other)
 plot!(p, data[1]["xAxisValues"], avg*ones(length(data[1]["xAxisValues"])), label="Average", linestyle=:dash)
 p
 savefig(p, "renewables proportion.png")
@@ -64,11 +67,7 @@ plot(renew./last)
 plot(coaletc./last)
 @show minimum(coaletc./last)
 @show maximum(coaletc./last)
-A = float(zeros(length(data[1]["xAxisValues"])))
-for i in [1:2; 4:17]
-    A .+= data[i]["data"]
-end
-plot((A)./last)
+plot((all_except_nuclear)./last)
 plot((coaletc .+ renew .+ other)./last)
 
 histogram(renew./last, label="Renewables/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
