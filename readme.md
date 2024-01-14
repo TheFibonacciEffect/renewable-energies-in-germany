b0536656c039669c54e8c23cfc32a46b662b527c
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-14 14:32:09 +0100

    make a histogram

--- readme.md
+++ readme.md
@@ -4,5 +4,6 @@ which is used for the website by the frauenhofer institute
 https://www.energy-charts.info/
 or more precisely
 https://www.energy-charts.info/charts/power/chart.htm?l=de&c=DE&source=public&year=2023&interval=year&stacking=stacked_percent_area&timeslider=0
-
+![Histogram](<renewables proportion histogram.png>)
+A histogram depicting the fraction of the power of renewable energies (Wind offshore, Wind onshore, Solar, Biomass, Hydro Run-of-River, Geothermal, Hydro water reservoir, Hydro pumped storage) (in MW) on the Eletronic load in Germany in 2023
 ![Proportion of renewables](<renewables proportion.png>)
--- vis.jl
+++ vis.jl
@@ -29,25 +29,29 @@ ws = [14,15,1      ]
 # "Anteil EE an der Erzeugung",             20
 # "Anteil EE an der Last",     21
 
-# for key in data|>keys
-#     source = data[key]
-#     @show name = source["name"]["de"] , key
-# end
+for key in [15,16,17,5,4,10,11,12,13]
+    source = data[key]
+    # @show name = source["name"]["de"] , key
+    println(source["name"]["en"])
+end
 
 coaletc = data[6]["data"] .+ data[7]["data"] .+ data[8]["data"] + data[9]["data"]
-renew = data[15]["data"] .+ data[16]["data"] .+ data[17]["data"] + data[5]["data"] .+ data[4]["data"] .+ data[10]["data"] .+ data[11]["data"] .+ data[12]["data"] .+ data[13]["data"]
+renew = data[15]["data"] .+ data[16]["data"] .+ data[17]["data"] + data[5]["data"] .+ data[4]["data"] .+ data[10]["data"] .+ data[11]["data"] .+ data[12]["data"]
 other = data[13]["data"]
-tot = coaletc .+ renew
+tot = coaletc .+ renew .+ other
 # plot!(p, data[1]["xAxisValues"], coaletc./tot, label="Coal etc")
 plot!(p, data[1]["xAxisValues"], renew./tot, label="Renewables/(Renewables+Coal+Gas)")
 @show avg = sum(renew)/sum(tot)
 @show min = minimum(renew./tot)
 plot!(p, data[1]["xAxisValues"], avg*ones(length(data[1]["xAxisValues"])), label="Average", linestyle=:dash)
+p
 savefig(p, "renewables proportion.png")
+
 ee_vs_last = data[21]["data"]
 min = minimum(data[21]["data"])
 maximum(data[21]["data"])
 last = data[18]["data"]
+
 
 plot(renew./last)
 @show minimum(renew./last)*(sum(last)/sum(renew))
@@ -64,3 +68,9 @@ for i in [1:2; 4:17]
 end
 plot((A)./last)
 plot((coaletc .+ renew .+ other)./last)
+
+histogram(renew./last, label="Renewables/(Electronic Load)",norm=:probability, bins=10)
+title!("Renewables vs electronic load in 2023")
+xlabel!("Proportion of the total energy demand per hour in 2023")
+ylabel!("Proportion of hours in 2023")
+savefig("renewables proportion histogram.png")
