b19993846eb79fb672bc33b89a6667293acfc967
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-14 15:53:51 +0100

    subtract the average for some of the sources

--- vis.jl
+++ vis.jl
@@ -31,6 +31,10 @@ for key in [1,15,16,17,5,4,10,11,12,13]
     println(source["name"]["en"], " ", key)
 end
 
+function average(data)
+    sum(data)/length(data)
+end
+
 # define the groups of the sources
 coaletc = data[6]["data"] .+ data[7]["data"] .+ data[8]["data"] + data[9]["data"]
 renew = data[15]["data"] .+ data[16]["data"] .+ data[17]["data"] + data[5]["data"] .+ data[4]["data"] .+ data[10]["data"] .+ data[11]["data"] .+ data[12]["data"]
@@ -39,6 +43,9 @@ wind_solar = data[15]["data"] + data[16]["data"] + data[17]["data"]
 wind_solar_storage = data[15]["data"] + data[16]["data"] + data[17]["data"]  + data[11]["data"] .+ data[12]["data"]+ data[1]["data"]
 combined_renew_coal_other = coaletc .+ renew .+ other
 resudallast = data[19]["data"] # https://de.wikipedia.org/wiki/Residuallast
+nuclear = data[3]["data"]
+# substitute nothing with NaNs
+nuclear = [x == nothing ? NaN : x for x in nuclear]
 
 all_except_nuclear = float(zeros(length(data[1]["xAxisValues"])))
 for i in [1:2; 4:17]
@@ -77,13 +84,13 @@ xlabel!("Proportion of the total energy demand per hour in 2023")
 ylabel!("Proportion of hours in 2023")
 savefig("renewables proportion histogram.png")
 
-histogram(wind_solar./last, label="(Wind + Solar)/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
+histogram(wind_solar./last .- average(wind_solar./last), label="(Wind + Solar)/(Electronic Load)",norm=:probability, bins=-0.5:0.2:0.5)
 title!("Wind + Solar vs electronic load in 2023")
 xlabel!("Proportion of the total energy demand per hour in 2023")
 ylabel!("Proportion of hours in 2023")
 savefig("ws proportion histogram.png")
 
-histogram(wind_solar_storage./last, label="(Wind + Solar + Storage)/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
+histogram(wind_solar_storage./last .- average(wind_solar_storage./last), label="(Wind + Solar + Storage)/(Electronic Load)",norm=:probability, bins=-0.5:0.2:0.5)
 title!("Wind + Solar + Storage vs electronic load in 2023")
 xlabel!("Proportion of the total energy demand per hour in 2023")
 ylabel!("Proportion of hours in 2023")
@@ -94,3 +101,9 @@ title!("Residual Load vs electronic load in 2023")
 xlabel!("Proportion of the total energy demand per hour in 2023")
 ylabel!("Proportion of hours in 2023")
 savefig("residual load proportion histogram.png")
+
+histogram(nuclear./last, label="nuclear/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
+title!("nuclear vs electronic load in 2023")
+xlabel!("Proportion of the total energy demand per hour in 2023")
+ylabel!("Proportion of hours in 2023")
+savefig("nuclear proportion histogram.png")
