9ce34e6b2f58f7243772c1afa079882bdb2a4853
Caspar Gutsche <caspargutsche1@gmail.com>
2024-01-14 14:51:58 +0100

    make more histograms

--- vis.jl
+++ vis.jl
@@ -32,12 +32,14 @@ ws = [14,15,1      ]
 for key in [15,16,17,5,4,10,11,12,13]
     source = data[key]
     # @show name = source["name"]["de"] , key
-    println(source["name"]["en"])
+    println(source["name"]["en"], " ", key)
 end
 
 coaletc = data[6]["data"] .+ data[7]["data"] .+ data[8]["data"] + data[9]["data"]
 renew = data[15]["data"] .+ data[16]["data"] .+ data[17]["data"] + data[5]["data"] .+ data[4]["data"] .+ data[10]["data"] .+ data[11]["data"] .+ data[12]["data"]
 other = data[13]["data"]
+wind_solar = data[15]["data"] + data[16]["data"] + data[17]["data"] 
+wind_solar_storage = data[15]["data"] + data[16]["data"] + data[17]["data"]  + data[11]["data"] .+ data[12]["data"]+ data[1]["data"]
 tot = coaletc .+ renew .+ other
 # plot!(p, data[1]["xAxisValues"], coaletc./tot, label="Coal etc")
 plot!(p, data[1]["xAxisValues"], renew./tot, label="Renewables/(Renewables+Coal+Gas)")
@@ -69,8 +71,20 @@ end
 plot((A)./last)
 plot((coaletc .+ renew .+ other)./last)
 
-histogram(renew./last, label="Renewables/(Electronic Load)",norm=:probability, bins=10)
+histogram(renew./last, label="Renewables/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
 title!("Renewables vs electronic load in 2023")
 xlabel!("Proportion of the total energy demand per hour in 2023")
 ylabel!("Proportion of hours in 2023")
 savefig("renewables proportion histogram.png")
+
+histogram(wind_solar./last, label="(Wind + Solar)/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
+title!("Wind + Solar vs electronic load in 2023")
+xlabel!("Proportion of the total energy demand per hour in 2023")
+ylabel!("Proportion of hours in 2023")
+savefig("ws proportion histogram.png")
+
+histogram(wind_solar_storage./last, label="(Wind + Solar + Storage)/(Electronic Load)",norm=:probability, bins=0:0.2:1.2)
+title!("Wind + Solar + Storage vs electronic load in 2023")
+xlabel!("Proportion of the total energy demand per hour in 2023")
+ylabel!("Proportion of hours in 2023")
+savefig("ws storage proportion histogram.png")
